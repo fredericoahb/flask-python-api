94cfeeb8008fbc26b289aeec31b41bd9f77d1cf6
Frederico Balieiro <fredericoahb@gmail.com>
2020-06-10 17:32:08 -0300

    Implementando cadastro de usuarios

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import User, UserRegister
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -13,6 +14,8 @@ def cria_banco():
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User, '/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
---
+++ resources/usuario.py
@@ -0,0 +1,29 @@
+from flask_restful import Resource
+from models.usuario import UserModel
+
+class User(Resource):
+
+    def get(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            return user.json()
+        return {'message': 'User not found.'}, 404
+
+    def delete(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            user.delete_user()
+            return {'message': 'User deleted.'}
+        return {'message': 'User not found.'}, 404
+
+class UserRegister(Resource):
+    # /cadastro
+    def post(self):
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "The login '{}' already exists.".format(dados['login'])}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User created successfully!'}, 201 # Created
