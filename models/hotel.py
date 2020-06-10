6f8493aa3cb4ce5dc637a7dd78d5c18e5ca80a4c
Frederico Balieiro <fredericoahb@gmail.com>
2020-06-10 16:08:03 -0300

    CRUD integrado com Banco de Dados

--- models/hotel.py
+++ models/hotel.py
@@ -8,7 +8,7 @@ class HotelModel(banco.Model):
     estrelas = banco.Column(banco.Float(precision=1))
     diaria = banco.Column(banco.Float(precision=2))
     cidade = banco.Column(banco.String(40))
-    
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
@@ -24,3 +24,24 @@ class HotelModel(banco.Model):
             'diaria': self.diaria,
             'cidade': self.cidade
         }
+
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id=hotel_id).first()
+        if hotel:
+            return hotel
+        return None
+
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hotel(self, nome, estrelas, diaria, cidade):
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+
+    def delete_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
