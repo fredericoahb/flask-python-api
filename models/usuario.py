92dea400468674362d1eab98198b8281e812c796
Frederico Balieiro <fredericoahb@gmail.com>
2020-06-12 14:13:40 -0300

    Adicionando logout de Usuarios

--- models/usuario.py
+++ models/usuario.py
@@ -7,8 +7,7 @@ class UserModel(banco.Model):
     login = banco.Column(banco.String(40))
     senha = banco.Column(banco.String(40))
 
-    def __init__(self, user_id, login, senha):
-        self.user_id = user_id
+    def __init__(self, login, senha):
         self.login = login
         self.senha = senha
 
@@ -16,20 +15,20 @@ class UserModel(banco.Model):
         return {
             'user_id': self.user_id,
             'login': self.login
-        }
+            }
 
     @classmethod
     def find_user(cls, user_id):
         user = cls.query.filter_by(user_id=user_id).first()
         if user:
-            return user.login
+            return user
         return None
 
     @classmethod
     def find_by_login(cls, login):
         user = cls.query.filter_by(login=login).first()
         if user:
-            return user.login
+            return user
         return None
 
     def save_user(self):
